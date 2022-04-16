952eb87067b57f8a0b1c7641d34299b3ea8c5954
liyanzu0926 <liyanzu0926@gmail.com>
2022-04-16 13:26:09 +0800

    日常提交

---
+++ src/niuke/top100/BM83.java
@@ -0,0 +1,28 @@
+package niuke.top100;
+
+import java.util.*;
+
+public class BM83g {
+    public String trans(String s, int n) {
+        // write code here
+        String[] arr = s.split(" ", -1);
+        StringBuffer sb = new StringBuffer();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(transform(arr[i]) + " ");
+        }
+        return sb.substring(0, sb.length() - 1);
+    }
+
+    private String transform(String s) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                sb.append(Character.toUpperCase(ch));
+            } else {
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        return sb.toString();
+    }
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -4,52 +4,9 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        String str = "basdfasdfasdfasdfas";
-        String pattern = "basdf.*asdf.*asdf.*asdf.*s";
-        System.out.println(match(str, pattern));
+        String s = "sdasd";
+        StringBuffer sb = new StringBuffer();
     }
 
-    public static boolean match(String str, String pattern) {
-        // write code here
-        int len1 = str.length();
-        int len2 = pattern.length();
-        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
-        dp[0][0] = true;
-        // 下面是为了初始化x*x*x*x*这种情况
-        for (int j = 2; j <= len2; j++) {
-            if (pattern.charAt(j - 1) == '*') {
-                dp[0][j] = dp[0][j - 2];
-            }
-        }
 
-        for (int i = 1; i <= len1; i++) {
-            for (int j = 1; j <= len2; j++) {
-                if (pattern.charAt(j - 1) != '*') {
-                    if (isMatch(str.charAt(i - 1), pattern.charAt(j - 1))) {
-                        dp[i][j] = dp[i - 1][j - 1];
-                    } else {
-                        dp[i][j] = false;
-                    }
-                } else {
-                    // 判断pattern '*'之前的字符是否和str当前字符匹配
-                    if (isMatch(str.charAt(i - 1), pattern.charAt(j - 2))) {
-                        // 如果上面匹配则继续用pattern ‘*’之前的字符和str当前字符的前一个字符比较
-                        // 或者把pattern '*'前面的字符看作出现0次
-                        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - 2];
-                    } else {
-                        dp[i][j] = dp[i][j - 2];
-                    }
-                }
-            }
-        }
-        return dp[len1][len2];
-    }
-
-    // 判断当前两个字符是否匹配
-    private static boolean isMatch(char ch1, char ch2) {
-        if (ch1 == ch2 || ch2 == '.') {
-            return true;
-        }
-        return false;
-    }
 }
