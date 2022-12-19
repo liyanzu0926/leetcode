2a1b177ea10e626ef18889a60df29cd717bea2a0
liyanzu0926 <liyanzu0926@gmail.com>
2022-12-19 17:32:46 +0800

    sql

---
+++ src/sql/TestMapper.java
@@ -0,0 +1,14 @@
+package sql;
+
+/**
+ * @Description
+ * @Author lyz
+ * @Date 2022/12/19 16:36
+ */
+public interface TestMapper {
+    String sql511();
+
+    String sql512();
+
+    String sql534();
+}
